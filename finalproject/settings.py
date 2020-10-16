7b8951c44ed6904d03f84bf0c3a0f069e47c1ce3
dnm2136 <dnm2136@columbia.edu>
2020-10-16 20:32:15 +0000

    Add settings.py and urls.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+DATE_INPUT_FORMATS = ['%m%d%Y']
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'squirrels',
 ]
 
 MIDDLEWARE = [
