ecbc568c2c215572ab5da599c18b603657dfb777
Luiza Loiferman <luizaloiferman@MacBook-Pro-5.local>
2020-10-21 16:14:26 -0400

    updated add better

--- squirrels/views.py
+++ squirrels/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from datetime import datetime
+
+from django.db.models import Max
 
 from .models import SquirrelDB
-from datetime import datetime
 
 def map(request):
     sightings = SquirrelDB.objects.all().filter(Primary_color = 'Black')
@@ -24,7 +26,9 @@ def detail(request, sighting_id):
     if request.method == "POST" and request.POST.get('submit_id') == "Update":
         #print(request.POST.get('Shift_id'))
         sighting.Shift = request.POST.get('Shift_id')
-        sighting.Lat_long = request.POST.get('Lat_long_id')
+        sighting.X = request.POST.get('long_id')
+        sighting.Y = request.POST.get('lat_id')
+        sighting.Lat_long = "POINT (%s %s)" % (sighting.Y, sighting.X)
         sighting.Date = datetime.strptime(request.POST.get('Date_id'), "%b. %d, %Y")
         sighting.Age = request.POST.get('Age_id')
         sighting.save()
@@ -33,13 +37,43 @@ def detail(request, sighting_id):
     }
     return render(request, 'squirrels/detail.html', context)
 
+
 def update(request):
+
     return HttpResponse('updates page')
 
 def add(request):
-    return HttpResponse('add page')
+    if request.method == "POST":
+        sighting = SquirrelDB()
+        sighting.id = SquirrelDB.objects.aggregate(Max("id"))['id__max']
+        sighting.sq_id = request.POST.get('sighting_sq_id')
+        sighting.Shift = request.POST.get('Shift_id')
+        sighting.X = request.POST.get('long_id')
+        sighting.Y = request.POST.get('lat_id')
+        sighting.Lat_long = "POINT (%s %s)" % (sighting.Y, sighting.X)
+        sighting.Date = datetime.strptime(request.POST.get('Date_id'), "%b. %d, %Y")
+        sighting.Age = request.POST.get('Age_id')
+        sighting.Running = True
+        sighting.Chasing = True
+        sighting.Climbing = True
+        sighting.Eating = False
+        sighting.Foraging = False
+        sighting.Kuks = False
+        sighting.Quass = False
+        sighting.Moans = False
+        sighting.Approaches = False
+        sighting.Tail_flag = False
+        sighting.Tail_twitch = False
+        sighting.Indifferent = False
+        sighting.Runs_from = False
+        sighting.save()
+    else:
+        sighting = SquirrelDB()
+    context = {
+            'sighting': sighting,
+    }
+
+    return render(request, 'squirrels/add.html', context)
 
 def stats(request):
     return HttpResponse('stats page')
-
-
