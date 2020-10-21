9ad4ab78464527e6190ac458da585a53fcd081c0
root <root@instance-8.c.my-project-python-289515.internal>
2020-10-21 19:25:45 +0000

    updated the views and detail to handle update

--- squirrels/views.py
+++ squirrels/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
 from .models import SquirrelDB
+from datetime import datetime
 
 def map(request):
     sightings = SquirrelDB.objects.all().filter(Primary_color = 'Black')
@@ -20,12 +21,17 @@ def sightings(request):
 
 def detail(request, sighting_id):
     sighting = get_object_or_404(SquirrelDB, pk=sighting_id)
-
+    if request.method == "POST" and request.POST.get('submit_id') == "Update":
+        #print(request.POST.get('Shift_id'))
+        sighting.Shift = request.POST.get('Shift_id')
+        sighting.Lat_long = request.POST.get('Lat_long_id')
+        sighting.Date = datetime.strptime(request.POST.get('Date_id'), "%b. %d, %Y")
+        sighting.Age = request.POST.get('Age_id')
+        sighting.save()
     context = {
             'sighting': sighting,
     }
     return render(request, 'squirrels/detail.html', context)
-
 
 def update(request):
     return HttpResponse('updates page')
@@ -35,3 +41,5 @@ def add(request):
 
 def stats(request):
     return HttpResponse('stats page')
+
+
